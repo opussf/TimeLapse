64f0bb293ac21ba21c0ab19547a356b346145237
opussf <github@zz9-za.com>
2019-07-10 01:12:56 -0700

    Minor changes

--- src/web/mythumb.php
+++ src/web/mythumb.php
@@ -3,7 +3,7 @@
 # Charles Gordon
 # 18 June 2004
 #########################################
-# Parameters: 
+# Parameters:
 # fname = Filename of image to thumbnail
 # w = desired width
 # h = desired height
@@ -33,16 +33,15 @@
 
   $log_str = "";
 
-  $log_file = "$name.log";
   $log_file = "mythumb_".date("md").".log";
- 
+
   #$new_width=100;
-  $new_height=75;  
+  $new_height=75;
 
   $image_name=$_GET['fname'];
   $width=$_GET['w'];
   $height=$_GET['h'];
-  $quiet=is_null($_GET['q']) ? false : true; 
+  $quiet=is_null($_GET['q']) ? false : true;
 
   $awidth =explode(",",$width);
   $aheight=explode(",",$height);
@@ -67,16 +66,16 @@
   $len = strlen($image_name);
   $pos = strrpos($image_name,".");
   $type = strtoupper(substr($image_name,$pos+1,$len));
-  
+
   # build this from the first elements
   # (controls the execution of the script)
   $thumb_name = make_thumb_name($image_name,$awidth[0],$aheight[0]);
-  
+
   $d = date("d M Y H:i:s");
   fwrite($fhLog,"$d\t$image_name\t");
-  
+
   thumb_image ($image_name);
-  
+
   function make_thumb_name($fname,$w,$h)
   ## makes and returns a thumb name
   {
@@ -87,7 +86,7 @@
     $thumb_name = $thumb_dir . "tn_".$w."x".$h."_".$fname;
     return $thumb_name;
   }
-  
+
   function set_w_h($img,$w,$h)
   ## returns true if thumb would be smaller than original
   {
@@ -108,11 +107,11 @@
     if (!empty($h)) {
       $new_height = $h;
     }
-    
+
     # calc the width if not set
-    #if (!isset($new_width)) $new_width = $new_height * $ratio;	
+    #if (!isset($new_width)) $new_width = $new_height * $ratio;
     if (empty($w)) $new_width = $new_height * $ratio;
-    
+
     #fwrite($fhLog, "Final Size: $new_width x $new_height\n");
     if (($new_width >= ImageSX($img)) or ($new_height >= ImageSY($img))) {
       #print ("Can't resize larger");
@@ -135,7 +134,7 @@
 
     $s = $new_width . "x" . $new_height;
     $s = $awidth[0] . "x" . $aheight[0];
-   
+
     # check for pre-existing thumb (use the first size given)
     if (file_exists($thumb_name)) {
       if (filesize($thumb_name) > 0) {		# if file larger than 0
