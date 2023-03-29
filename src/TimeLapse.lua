2d7a8791eaaeaa4b7d6aef178f00fde9bfdc5270
opussf <github@zz9-za.com>
2023-03-28 17:57:05 -0700

    Format

--- src/TimeLapse.lua
+++ src/TimeLapse.lua
@@ -100,7 +100,7 @@ function TL.PrintHelp()
 	end
 end
 function TL.Status()
-	TL.Print( "Screen Capture every "..SecondsToTime(TL_Options.Delay,false,false,5).."("..( TL_Options.Enabled and "Enabled" or "Disabled" )..")" )
+	TL.Print( "Screen Capture every "..SecondsToTime(TL_Options.Delay,false,false,5).." ("..( TL_Options.Enabled and "Enabled" or "Disabled" )..")" )
 end
 -- this needs to be at the end because it is referencing functions
 TL.CommandList = {
